250545a7cb002e426bd46e12ff124a8e6555003d
Alexander Saprykin <cutwatercore@gmail.com>
2020-03-31 17:26:13 +0200

    Migrate to galaxy ng

--- production/postgres/postgresql-init/10-init-galaxy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-psql --set dbname="${GALAXY_DB_NAME:-galaxy}" \
-     --set username="${GALAXY_DB_USER:-galaxy}" \
-     --set password="${GALAXY_DB_PASSWORD}" \
-<<EOF
-  CREATE ROLE :"username" WITH LOGIN PASSWORD :'password';
-  CREATE DATABASE :"dbname" OWNER :"username";
-EOF
--- production/postgres/postgresql-init/10-init-pulp.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-psql --set dbname="${PULP_DB_NAME:-pulp}" \
-     --set username="${PULP_DB_USER:-pulp}" \
-     --set password="${PULP_DB_PASSWORD}" \
-<<EOF
-  CREATE ROLE :"username" WITH LOGIN PASSWORD :'password';
-  CREATE DATABASE :"dbname" OWNER :"username";
-EOF
